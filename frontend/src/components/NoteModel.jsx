d5e6664bbb741c6168b5e77b4007dcf1e1ebe65a
prapti2385 <praptikhandelwal14@gmail.com>
2025-11-18 18:23:41 +0530

    Note Model frontend done

---
+++ frontend/src/components/NoteModel.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState } from "react";
+
+const NoteModel = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center">
+      <div className="bg-white p-8 rounded">
+        <h2 className="text-xl font-bold mb-4">Add New Note</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Note Title"
+            className="border p-2 w-full mb-4"
+          />
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Note Description"
+            className="border p-2 w-full mb-4"
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+          >
+            Add Note
+          </button>
+        </form>
+        <button className="mt-4 text-red-500">Cancel</button>
+      </div>
+    </div>
+  );
+};
+
+export default NoteModel;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import NoteModel from "../components/NoteModel";
+import { useState } from "react";
 
 const Home = () => {
+  const [isModelOpen, setModelOpen] = useState(false);
   return (
-    <div>
+    <div className="bg-gray-100 min-h-screen">
       <Navbar />
+      <button
+        className="bg-teal-500 text-white font-bold p-4 rounded fixed right-4 bottom-4 text-2xl"
+        onClick={() => setModelOpen(true)}
+      >
+        +
+      </button>
+      {isModelOpen && <NoteModel />}
     </div>
   );
 };

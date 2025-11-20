b4f340f51ee257d410b6260373df907a75979cda
prapti2385 <praptikhandelwal14@gmail.com>
2025-11-20 11:48:16 +0530

    Update MongoDB connection string to use Atlas cluster

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/note_app");
+    await mongoose.connect(
+      "mongodb+srv://prapticodes_db_user:VnttajlgO1WTkPZ2@cluster0.illdhf6.mongodb.net/?appName=Cluster0"
+    );
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Error connecting to MongoDB", error.message);
